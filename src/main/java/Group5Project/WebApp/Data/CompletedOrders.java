0839a0d57443e6db710b67bacfe779ea93876f26
shea cooke <SheaSCooke@gmail.com>
2022-11-06 16:17:36 -0500

    changing collections completed

---
+++ src/main/java/Group5Project/WebApp/Data/CompletedOrders.java
@@ -0,0 +1,12 @@
+package Group5Project.WebApp.Data;
+
+import Group5Project.WebApp.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//will be deleted once we integrate SQLLite
+public class CompletedOrders {
+
+    public static List<Order> CompletedOrdersList = new ArrayList<Order>();
+}
--- src/main/java/Group5Project/WebApp/controller/PastAndPendingOrdersController.java
+++ src/main/java/Group5Project/WebApp/controller/PastAndPendingOrdersController.java
@@ -1,10 +1,16 @@
 package Group5Project.WebApp.controller;
 
+import Group5Project.WebApp.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
+import java.util.UUID;
 
+import static Group5Project.WebApp.Data.Cart.ItemsInCart;
+import static Group5Project.WebApp.Data.CompletedOrders.CompletedOrdersList;
 import static Group5Project.WebApp.Data.PendingOrders.CurrentPendingOrders;
 
 @Controller
@@ -13,6 +19,26 @@ public class PastAndPendingOrdersController {
     @GetMapping("PastAndPendingOrders")
     public String PastAndPendingOrders (Map<String, Object> model) {
         model.put("CurrentPendingOrders", CurrentPendingOrders);
+        model.put("CompletedOrdersList", CompletedOrdersList);
         return "PastAndPendingOrders";
     }
+
+    @GetMapping("/CompletedOrder/{ID}")
+    public String CompletedOrder(@PathVariable UUID ID) {
+
+        //get Order object
+        Order order = CurrentPendingOrders.stream().filter(i -> i.ID.equals(ID)).findFirst().get();
+
+        //remove from Pending orders
+        CurrentPendingOrders.removeIf(x -> x.ID.equals(ID));
+
+        //add to completed orders
+
+        CompletedOrdersList.add(order);
+
+
+        return "redirect:/PastAndPendingOrders";
+    }
+
+
 }
