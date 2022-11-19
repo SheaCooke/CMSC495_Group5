523fddb369449f5410899e5e9c6258eee594461e
shea cooke <SheaSCooke@gmail.com>
2022-11-19 14:50:13 -0500

    login and logout working

---
+++ src/main/java/Group5Project/WebApp/controller/LoginController.java
@@ -0,0 +1,39 @@
+package Group5Project.WebApp.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+
+    //Other code
+
+    @PostMapping("/login_success_handler")
+    public String loginSuccessHandler() {
+        //perform audit action
+        return "/";
+    }
+
+    @PostMapping("/login_failure_handler")
+    public String loginFailureHandler() {
+        //perform audit action
+        return "Login";
+    }
+
+    @RequestMapping(value="/Logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "Login";
+    }
+}
--- src/main/java/Group5Project/WebApp/controller/WebSecurityConfig.java
+++ src/main/java/Group5Project/WebApp/controller/WebSecurityConfig.java
@@ -3,16 +3,18 @@ package Group5Project.WebApp.controller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter */{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,9 +25,14 @@ public class WebSecurityConfig {
                 )
                 .formLogin((form) -> form
                         .loginPage("/Login")
+                                .loginProcessingUrl("/process-login")
+                                .defaultSuccessUrl("/")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout(logout -> logout
+                        .logoutUrl("/Logout")
+                        .logoutSuccessUrl("/Login")
+                        .invalidateHttpSession(true));
 
         return http.build();
     }
@@ -41,4 +48,38 @@ public class WebSecurityConfig {
 
         return new InMemoryUserDetailsManager(user);
     }
+//
+//    @Override
+//    protected void configure(final HttpSecurity http) throws Exception {
+//
+//        //@formatter:off
+//        http.authorizeRequests()
+//                .antMatchers("/Login").permitAll()
+//                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+//                .and()
+//                .formLogin()
+//                .loginPage("/Login")
+//                .loginProcessingUrl("/process-login")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login?error=true")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login?logout=true")
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID")
+//                .permitAll()
+//                .and()
+//                .csrf()
+//                .disable();
+//        //@formatter:on
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring()
+//                .antMatchers("/resources/**", "/static/**");
+//    }
+
 }
